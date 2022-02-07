a2f4cab200982f8501228261ad4687fc10589c5d
Zoe Huang <kc38580821@gmail.com>
2022-02-07 22:00:03 +0800

    add elif and correction comment

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -4,3 +4,15 @@ weight = float(weight)
 height = float(weight)
 BMI = weight/(height*height)
 print("您的BMI為", BMI)
+if BMI < 18.5 :                    #要記得if最後要加上冒號
+    print("體重過輕 Underweight.")  # print前記得要空四格
+elif BMI >= 18.5 and BMI < 24 :             
+    print("正常範圍 Healthy Weight") #and 後面的BMI還是要加
+elif BMI >= 24 and BMI < 27 :       #elif最後面還是要加上冒號
+    print("過重 Overweight.")
+elif BMI >= 27 and BMI < 30 :
+    print("輕度肥胖 Slightly Obesity.")
+elif BMI >=30 and BMI <35 :
+    print("中度肥胖 Obesity")
+else :                             #else最後面還是要加上冒號，print一樣要打在下一行 
+    print("重度肥胖 Severe Obesity")    
