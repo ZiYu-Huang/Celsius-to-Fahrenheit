6f599505581d2ed20a8f5235c21db18bc61bce03
Zoe Huang <kc38580821@gmail.com>
2022-02-12 21:02:11 +0800

    raise Systemexit

--- age_drive_regonition.py
+++ age_drive_regonition.py
@@ -1,6 +1,10 @@
 age = input("請問你今年幾歲?")
 age = int(age)                    #記得要Casting才能=
 drive = input("請問你開過車嗎?")
+if drive != "有" and drive != "沒有":
+    print("只能回答有或沒有喔")     #記得縮排 #被觸發錯誤用
+    raise SystemExit              #直接讓程式終止
+    #思考:要問到他回答有或沒有的loop要怎麼寫?hint:while loop
 if drive == "有":                 #雙層if #記得要冒號
     if age >= 18:                 #記得要冒號
         print("那你應該是合法駕駛") #記得要縮排
@@ -9,7 +13,3 @@ if drive == "有":                 #雙層if #記得要冒號
 elif drive == "沒有" or drive == "無":
     if age >= 18:
         print("那你可以去考駕照囉~")
-    else:
-        print("再等幾年就可以考駕照囉~")
-else:
-    print("只能回答有或沒有喔")
