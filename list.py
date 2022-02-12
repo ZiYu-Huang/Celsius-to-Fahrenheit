b4f2241a8753168f32007b9468f7a77e33c52c0d
Zoe Huang <kc38580821@gmail.com>
2022-02-12 17:34:28 +0800

    introduction

---
+++ list.py
@@ -0,0 +1,15 @@
+# list清單，符號是中括號
+# 清單就是用來裝東西的，且可以混裝int, float, bool, str不同資料型別
+# 清單List可以想像成火車，每節車廂是index索引，編號從0開始
+# 用.append()可以加東西到清單裡
+# 用len()來取長度
+# 用in 來檢查東西在不在裡面
+
+a = ['Toyota', 'Honda'] #空清單
+print(a)
+print(a[0])
+print(a[1])
+a.append('Audi')        # 增加Audi到清單內
+print(a[2])
+print(len(a))           #取長度length
+print ('Audi' in a)     # 是非題 True, False
