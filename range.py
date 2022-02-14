d64b2aa06d708148ab78812b2a8f3b1f9b103e92
Zoe Huang <kc38580821@gmail.com>
2022-02-14 23:17:37 +0800

    intro

---
+++ range.py
@@ -0,0 +1,23 @@
+# python內建清單產生器:range
+# 自動從0開始產生，所以結尾值不包含
+# 通常只是為了拿來重複執行固定次數
+# 可以把i在for loop內容中印出來，就知道執行到第幾回
+
+range(3)   #[0, 1, 2] #自動從0開始，結尾值不包含，所以只到2
+
+for i in range(3):             #把清單range(3)中的東西一個一個拿出來，每一個稱呼為i
+    print(i)
+for i in range(3):
+    print('hi')                #hi會出現三次# 大部分寫這種for i in range的for loop只是為了拿來重複執行固定次數
+
+import random
+for i in range(3):
+    r = random.randint(1, 1000)
+    print(r)
+
+
+for i in range(5):
+    r = random.randint(1, 1000)
+    print('這是第', i + 1, '次產生隨機變數: ', r) #為了從1開始，所以要i+1
+
+# 所以要把東西執行x次，就用for i in range(x)
