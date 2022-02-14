4e62c9052ed582dcdf5b6b0c67a9f39da048d419
Zoe Huang <kc38580821@gmail.com>
2022-02-14 20:51:22 +0800

    better correction

--- review_analytics.py
+++ review_analytics.py
@@ -24,19 +24,17 @@ print('留言平均長度為:', sum_len / len(data), '筆')    # 要寫在for lo
 # Q:要怎麼篩選長度小於一百個字的?*這裡的'字'是指'character'ex.the有三個字
 
 new =[]
-with open('reviews.txt', 'r') as f:
-    for h in f:                        # 從f這個清單裡面一個一個拿出來叫h，h是一個清單 # 先從for loop一筆一筆讀取資料
-        if len(h) < 100:               # 記得後面要加冒號
-            new.append(h)
+for d in data:                        # 從f這個清單裡面一個一個拿出來叫h，h是一個清單 # 先從for loop一筆一筆讀取資料
+        if len(d) < 100:               # 記得後面要加冒號
+            new.append(d)
 print('一共有', len(new), '筆資料留言') # 要寫在for loop外面，寫在裡面的話，他會一直印 # 不是len(h)
 print(new[0])                          # 看看new清單中的第一筆資料
 
 
 # Q:篩選出有'good'的留言有幾筆
 good = []
-with open('reviews.txt', 'r') as f:
-    for g in f:
-        if 'good' in g:               # if後面接是非題 # good是字串記得加''
-            good.append(g) 
+for d in data:
+        if 'good' in d:               # if後面接是非題 # good是字串記得加''
+            good.append(d) 
 print('一共有', len(good), '筆留言提到good')
 print(good[0])
