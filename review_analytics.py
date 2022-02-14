ecea1c2b23f95c36499f795473d52ef125053cdf
Zoe Huang <kc38580821@gmail.com>
2022-02-14 20:34:50 +0800

    add filter-smaller than 100 characters

--- review_analytics.py
+++ review_analytics.py
@@ -10,7 +10,7 @@ with open('reviews.txt', 'r') as f:
         count += 1                     # count = count + 1
         if count % 1000 == 0:          # '%'是求餘數，也就是count要跟1000去求餘數等於零者 #記得if後面加冒號
             print(len(data))           # 讀取檔案的過程中，每一千筆印出
-print('第一筆留言為:', data[0])
+print('第一筆留言為:', data[0])         # 要寫在for loop外面，寫在裡面的話，他會一直印          
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
 # Q:要怎麼知道這一百萬筆留言平均長度?
@@ -19,5 +19,14 @@ sum_len = 0
 with open('reviews.txt', 'r') as f:
     for d in data:                    # 字串可以當清單求長度
         sum_len += len(d)             # sum_len = sum_len + len(d)
-avg = sum_len / len(data)
-print('留言平均長度為:', avg, '筆')
+print('留言平均長度為:', sum_len / len(data), '筆')    # 要寫在for loop外面，寫在裡面的話，他會一直印
+
+# Q:要怎麼篩選長度小於一百個字的?*這裡的'字'是指'character'ex.the有三個字
+
+new =[]
+with open('reviews.txt', 'r') as f:
+    for h in f:                        # 從f這個清單裡面一個一個拿出來叫h，h是一個清單 # 先從for loop一筆一筆讀取資料
+        if len(h) < 100:               # 記得後面要加冒號
+            new.append(h)
+print('一共有', len(new), '筆資料留言') # 要寫在for loop外面，寫在裡面的話，他會一直印 # 不是len(h)
+print(new[0])                          # 看看new清單中的第一筆資料
