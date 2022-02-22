d11cd904472429089b48f4cbb796ee31cd083e35
Zoe Huang <kc38580821@gmail.com>
2022-02-22 21:29:22 +0800

    basic version

--- products.py
+++ products.py
@@ -8,14 +8,19 @@ while True:
     if name == 'q':
         break
     price = input('請輸入商品價格: ') #寫第一個break下面，才不用前面商品不輸入了還再問一次
-    #p = [] 小清單
-    #p.append(name)
+    #p = [] 小清單        # 較直覺的寫法:先建立小清單p
+    #p.append(name)       # 再把兩個屬性加到p中
     #p.append(price)
-    #products.append(p) 小清單都放到大清單裡面，完成2維清單
-    products.append([name, price]) #上面四行簡潔成一行就可以了 #記得裡面要放中括號
-print(products)
+    #products.append(p)   #最後把小清單p加到大清單products中 #小清單都放到大清單裡面，完成2維清單
+    products.append([name, price]) #上面四行簡潔成一行就可以了 #記得裡面要放中括號 #兩個屬性直接建立小清單，放到大清單products中
+
+print(products)               # [['ramen', '100'], ['pasta', '200']]
 
 #清單的索引標籤叫做index
-print(products[0][0])    #先把大清單中的第0格拿出來，再拿出小清單中的第0格
+print(products[0][0])    # 先找出大清單中的第0格，再找到小清單中的第0格 #ramen
 
-
+# Q: 寫出每個商品對應的價格
+for p in products:
+    print(p[0], '的商品價格為: ', p[1])
+    print(p)     #是把每一個小清單拿出來
+    print(p[0])  #是把每一個小清單中的第0格拿出來
