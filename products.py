88f0d334e650cbc33fa54994fdde2299fa2fbfa1
Zoe Huang <kc38580821@gmail.com>
2022-02-25 21:36:37 +0800

    first refactor

--- products.py
+++ products.py
@@ -1,62 +1,54 @@
-#讓使用者重複地輸入商品名稱(購買過的東西)及商品價格
-#因為要重複輸入，所以用while loop比較合適(不知道會執行多少次用)，比起for loop(固定次數執行)
-#因為要對應名稱和價格，所以是二維清單
-#二維清單即大火車內有各個小小清單組成
+#先逐區塊設def，再把全部function都收起來
+#再依各def區塊決定function要不要設參數和回傳
 
-#讀取檔案程式碼
-#Q:先檢查有沒有檔案?
-import os #os = operating system作業系統模組，就像是政府一樣，可以綜覽全部電腦檔案
-products = []
-if os.path.isfile('products.csv'):  # 使用os.path.isfile()來檢查檔案在不在
-    print ('yeah!找到檔案了!')
-    with open ('products.csv', 'r', encoding= 'utf-8') as f: #讀取也會有亂碼問題，故加上encoding = 'utf-8'來讀取
-        for line in f:    
-            # s = line.strip()split(',')                  # .split(',') 用逗點分割line，遇到逗點就切 # 用.strip( )來除掉換行符號\n #split切割完的結果是清單，存到清單s中
-            # name = s[0]
-            # price = s[1]
-            # Q :讀取檔案時，如何不要把商品，價格寫入清單products中?
-            if '商品,價格' in line:                        #continue跟break一樣只能寫在迴圈中 #continue通常都是寫在迴圈中很高的位置，因為在低位置都快跑完這一回迴圈了                     
-                continue                                  #continue就是跳到下一迴的意思，以這個例子來說，剩下的16-17行就跳過不執行，直接從下一迴重來，沒有跳出迴圈(break才會跳出)，下一迴就不會是商品價格了
-            name, price = line.strip().split(',')         # 切割完分成兩塊，直接把切割完左右兩塊內容分別裝到name和price中，直接簡化前面三行   
-            products.append([name, price])                # 有了兩個屬性name和product，把讀到的內容裝進清單products中
-    print(products)                                       # 確定有讀取成功
-else:
-    print('找不到檔案')
+#讀取檔案
+import os
+def read_file(filename):     #參數filename沒有'' #插入這行進去後，下面全選插入一個tab縮進去，出現了tab跟空白鍵的混用#加入參數filename
+    products = []
+    if os.path.isfile(filename): #參數filename沒有''
+        print ('yeah!找到檔案了!')
+        with open (filename, 'r', encoding= 'utf-8') as f: #參數沒有''
+            for line in f:    
+                if '商品,價格' in line: 
+                    continue                                  
+                name, price = line.strip().split(',')          
+                products.append([name, price])                
+        print(products)                                       
+    else:
+        print('找不到檔案…')
+    return products         #回傳裝著我們讀完的資料 #回傳不用()
+
 #讓使用者輸入
-while True:
-    name = input('請輸入商品名稱: ')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格: ') #寫第一個break下面，才不用前面商品不輸入了還再問一次
-    #p = [] 小清單        # 較直覺的寫法:先建立小清單p
-    #p.append(name)       # 再把兩個屬性加到p中
-    #p.append(price)
-    #products.append(p)   #最後把小清單p加到大清單products中 #小清單都放到大清單裡面，完成2維清單
-    products.append([name, price]) #上面四行簡潔成一行就可以了 #記得裡面要放中括號 #兩個屬性直接建立小清單，放到大清單products中
-print(products)               # [['ramen', '100'], ['pasta', '200']]
+def user_input(products):       #要加上prodcuts這個參數，不然他要伸手去外面拿才知道
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格: ') 
+        products.append([name, price])  
+    print(products) 
+    return products             #回傳不用()
 
-#清單的索引標籤叫做index
-print(products[0][0])    # 先找出大清單中的第0格，再找到小清單中的第0格 #ramen
 
-# Q: 印出所有商品對應的價格
-for p in products:
-    print(p[0], '的商品價格為: ', p[1])
-    print(p)     #是把每一個小清單拿出來
-    print(p[0])  #是把每一個小清單中的第0格拿出來
-
-# Q: 如何把商品和價格儲存到電腦中(寫入檔案)?
-#加法可以做字串的合併，'我的年齡是' + '23' = 我的年齡是23
-with open ('products.csv', 'w')  as f:              #有沒有products.csv這個檔案不重要，有的話會覆蓋掉，沒有會自動建立(因為現在是寫入模式) #每一種商品有很多種不同屬性的時候，多會用.csv儲存資料
+# 印出所有商品對應的價格
+def print_products(products):
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n') #用.write()寫到檔案內，沒有這行的話還是沒有寫入檔案 # \n是換行符號，寫入多會在最後做換行 #csv檔多是以逗點做區隔，所以要在每個屬性中間用逗點做合併 
-#若上面price = int(price) 被寫成整數而非字串時，p[1]要用str()轉成字串，因為p[1]在上面已經改成int(price)了 ，而加法只能字串跟字串或整數跟整數
+        print(p[0], '的商品價格為: ', p[1]) 
+#沒有改變東西也沒有增加什麼，只是印出東西，所以不用回傳，只需要傳遞進去(設參數)
 
-# Q: 如何在檔案中加入欄位名稱?
-#add header #加入程式碼來寫header欄位
-with open ('products.csv', 'w', encoding = 'utf-8') as f:  #解決亂碼問題:用encoding編碼轉換 #utf-8 是最廣泛使用的編碼 #寫入和讀取檔案時，都會有編碼問題，可以用encoding= 'utf-8'解決中文字變亂碼的問題
-    f.write('商品,價格\n')                                 #在for loop前面先寫入欄位名稱   
-    for p in products:                                    #之後再一個一個各寫入一行
-        f.write(p[0] + ',' + str(p[1]) + '\n')            #用.write()寫到檔案內  # 加法只能字串跟字串，但p[0],p[1]要用str()轉成字串 #改存成.csv格式 # \n就是換行符號
+
+# 寫入檔案並在檔案中加入欄位名稱
+def write_file(filename, products):                      #參數filename沒有''
+    with open (filename, 'w', encoding = 'utf-8') as f:  #參數filename沒有''
+        f.write('商品,價格\n')                                  
+        for p in products:                                    
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+#沒有改變東西也沒有增加什麼，只是寫到檔案，所以不用回傳，只需要傳遞進去(設參數)
+
+products = read_file('products.csv')   #有return的話，function的執行結果就可以存下來開
+products = user_input(products)        #又存回products取代它原本的
+print_products(products)
+write_file('products.csv', products)
 # 剛剛有說，寫入和讀取都會有編碼問題，即使寫入encoding = 'utf-8'，excel讀取要是沒有使用utf-8，也還是會出現亂碼
 #  如何用excel正確使用utf-8編碼導入csv檔
 # Ecel導入csv資料:
